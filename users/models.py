da02afa60517ac8f88f5dece6044d40066c0ae3b
jjdahye <jdh020213@naver.com>
2024-04-14 23:03:42 +0900

    first commit

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    nickname = models.CharField(max_length=10, null=True)
+    image = models.ImageField(upload_to='profile/', null=True)
+
+class Meta :
+    db_table = 'profile'
+
+def _str_(self):
+    return self.nickname
+
